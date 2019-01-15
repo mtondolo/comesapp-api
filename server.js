266061ce6494de0d5c3ed6587a9dfba1fa67cb09
mtondolo <mtondolo@gmail.com>
2019-01-15 11:15:39 +0200

    Add get endpoint to return contacts data in json

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
 // Allows us to latest ES features
 'use strict';
 
-// Import http from node.js
-const http = require('http');
+// Import express module from node.js
+const express = require('express');
+
+// Initialize express
+const app = express();
+
+// Convert payload into a usable request body 
+const bodyParser = require('body-parser');
+
+// Allow api to take request from other domains
+const cors = require('cors');
+
+// Listen to url request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Receive requets from other domains
+app.use(cors());
+
+// Import data json file
+let contacts = require('./data');
+
+// Listen to http get request and handle it
+app.get('/api/contacts', (request, response) => {
+
+    // Response with message if no dota is found
+    if (!contacts) {
+        response.status(404).json({ message: 'No contacts found.' });
+      }
+
+   // Response data as contacts and header as json  
+   response.json(contacts);
+});
 
 // Include hostname and port
 const hostname = 'localhost';
 const port = 3001;
 
-// Kick off and start server
-const server = http.createServer((request, response) => {
-
-    // Status code for server response
-    response.statusCode = 200;
-
-    // Set plain text type response to serve to user 
-    response.setHeader('Content-Type', 'text/plain');
-
-    // Serve actual data to user
-    response.end('Hello World\n');
-});
-
 // Listen to incoming request through port and host
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
 
     // Log message to console after firing up the server
     console.log(`Server running at http://${hostname}:${port}/`);
