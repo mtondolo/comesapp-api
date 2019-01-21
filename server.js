6465e6627bbe66ca512c8c02f8dc0d9500d9c96d
mtondolo <mtondolo@gmail.com>
2019-01-21 12:36:42 +0200

    Add nodemom dependency

--- server.js
+++ server.js
@@ -12,28 +12,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const mongodbUri = 'mongodb://mtondolo:l0k0!010@ds159624.mlab.com:59624/mt110179';
-
-const mongooseUri = uriUtil.formatMongoose(mongodbUri);
-const dbOptions = {};
+const MONGOLAB_URI = 'mongodb://mtondolo:l0k0!010@ds159624.mlab.com:59624/mt110179';
+//const mongooseUri = uriUtil.formatMongoose(mongodbUri);
+//const dbOptions = {};
 
 app.use('/api/products', require('./api/products/routes/post_product'));
 app.use('/api/products', require('./api/products/routes/get_products'));
 
 //const hostname = 'localhost';
 //const port = 3001;
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-const server = app.listen(port  => {
+const server = app.listen(port, () => {
 
-  mongoose.connect(mongooseUri, dbOptions, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(`Server running at http://${localhost}:${port}/`);
-
-  });
+  mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
   
 });
