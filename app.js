de8054a410e182c83289468ac666ad7f34a283d7
mtondolo <mtondolo@gmail.com>
2019-01-22 11:41:25 +0200

    Remove port from api

--- app.js
+++ app.js
@@ -6,14 +6,14 @@
     app.controller('productController', function($scope, $http) {
 
       // Get the product from the endpoint
-      $http.get('https://beznet-api.herokuapp.com:8080/api/products')
+      $http.get('https://beznet-api.herokuapp.com/api/products')
         .then(function(response) {
           $scope.products = response.data;
         });
 
         // Save the product to the new endpoint
         $scope.saveProduct = function(product) {
-            $http.post('https://beznet-api.herokuapp.com:8080/api/products', product)
+            $http.post('https://beznet-api.herokuapp.com/api/products', product)
               .then(function(response) {
                 $scope.products.push(response.data);
             });
