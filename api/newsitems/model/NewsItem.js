f71c8de119fb8e3656561db553f14a50fe7781b9
mtondolo <mtondolo@gmail.com>
2019-01-24 13:13:25 +0200

    Change mongooese schema properties

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const NewsItemSchema = new Schema({
-    headline: { type: String, required: true },
-    story: { type: String, required: true },
-    author: { type: String, required: true },
-    image: { type: String, required: true }   
+    headline: { type: String, required: false },
+    story: { type: String, required: false },
+    author: { type: String, required: false },
+    image: { type: String, required: false }   
 });
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
