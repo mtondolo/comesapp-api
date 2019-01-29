c04084a440ae865ed75e60249f4d278c97c39649
mtondolo <mtondolo@gmail.com>
2019-01-29 14:04:54 +0200

    Change image data type to mongoose type url

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+require('mongoose-type-url');
 const Schema = mongoose.Schema;
 
 const NewsItemSchema = new Schema({
     headline: { type: String, required: true },
     story: { type: String, required: true },
     author: { type: String, required: true },
-    image: { type: String, required: true }   
+    image: { type: mongoose.SchemaTypes.Url, required: true }      
 });
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
