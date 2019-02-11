c095bce5fbae035886b4ac1e58e778faabf4a9c2
mtondolo <mtondolo@gmail.com>
2019-02-11 12:04:42 +0200

    Change image and story data types to url

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const NewsItemSchema = new Schema({
     headline: { type: String, required: true },
-    story: { type: String, required: true },
+    storyUrl: { type: mongoose.SchemaTypes.Url, required: true },
     date: { type: String, required: true },
-    image: { type: mongoose.SchemaTypes.Url, required: true }      
+    imageUrl: { type: mongoose.SchemaTypes.Url, required: true }      
 });
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
