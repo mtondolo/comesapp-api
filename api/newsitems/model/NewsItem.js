2dd34380151dcc9ca180eb6bcae447dc4a3019fc
mtondolo <mtondolo@gmail.com>
2019-02-25 09:31:38 +0200

    Add time stamp

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -6,8 +6,10 @@ const NewsItemSchema = new Schema({
     headline: { type: String, required: true },
     story: [{ type: String, required: true }],
     storyUrl: { type: mongoose.SchemaTypes.Url, required: true },  
-    date: { type : Date, default: Date.now }, 
+    date: { type : Date, default: Date.now },
+    timestamps: true, 
     imageUrl: { type: mongoose.SchemaTypes.Url, required: true }      
 });
+
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
