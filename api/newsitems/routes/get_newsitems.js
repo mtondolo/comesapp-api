98ef19579b444b23d29514746c9c8d47a01ffbac
mtondolo <mtondolo@gmail.com>
2019-02-25 13:45:21 +0200

    Rechange sort order to desc

--- api/newsitems/routes/get_newsitems.js
+++ api/newsitems/routes/get_newsitems.js
@@ -12,7 +12,7 @@ router.route('/')
         res.status(400).json(err);
       }
       res.json(newsitems);
-    }).sort({created_At: 1}); 
+    }).sort({created_At: -1}); 
 
   });
 module.exports = router;
