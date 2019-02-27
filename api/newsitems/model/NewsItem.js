97167be3ff08696cbb0eef1c6e5942f3b8d4456b
mtondolo <mtondolo@gmail.com>
2019-02-27 17:03:51 +0200

    Revert to stable date code

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 require('mongoose-type-url');
-timestamps = require('mongoose-timestamp')
 const Schema = mongoose.Schema;
 
 const NewsItemSchema = new Schema({
     headline: { type: String, required: true },
     story: [{ type: String, required: true }],
-    storyUrl: { type: mongoose.SchemaTypes.Url, required: true },   
-    imageUrl: { type: mongoose.SchemaTypes.Url, required: true }    
-      
-}, 
-
-{ timestamps: { createdAt: 'created_at' } });
+    storyUrl: { type: mongoose.SchemaTypes.Url, required: true },  
+    created_At: { type: Date, default: Date.now},
+    imageUrl: { type: mongoose.SchemaTypes.Url, required: true }      
+});
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
