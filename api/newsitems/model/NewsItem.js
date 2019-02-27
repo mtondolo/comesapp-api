b539bb36c3b19156aaac48df78681ef9d80bb3c7
mtondolo <mtondolo@gmail.com>
2019-02-27 16:14:56 +0200

    Add collection name and timestamps

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 require('mongoose-type-url');
+timestamps = require('mongoose-timestamp')
 const Schema = mongoose.Schema;
 
 const NewsItemSchema = new Schema({
     headline: { type: String, required: true },
     story: [{ type: String, required: true }],
-    storyUrl: { type: mongoose.SchemaTypes.Url, required: true },  
-    created_At: { type: Number, default: new Date },  
+    storyUrl: { type: mongoose.SchemaTypes.Url, required: true },   
     imageUrl: { type: mongoose.SchemaTypes.Url, required: true }    
       
-});
+}, { collection: 'news' });
+
+NewsItemSchema.plugin(timestamps)
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
