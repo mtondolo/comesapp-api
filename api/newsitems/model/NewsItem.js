e63e24212d9c8dd6d2a1b2aa3fa367cf10ce8db5
mtondolo <mtondolo@gmail.com>
2019-02-28 15:19:12 +0200

    Get timestamp with mongoose-common-plugin

--- api/newsitems/model/NewsItem.js
+++ api/newsitems/model/NewsItem.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 require('mongoose-type-url');
-const createdPlugin = require('mongoose-plugin-created');
+const mongooseCommonPlugin = require('mongoose-common-plugin');
 const Schema = mongoose.Schema;
 
 const NewsItemSchema = new Schema({
     headline: { type: String, required: true },
     story: [{ type: String, required: true }],
-    storyUrl: { type: mongoose.SchemaTypes.Url, required: true },   
-    imageUrl: { type: mongoose.SchemaTypes.Url, required: true },        
+    storyUrl: { type: mongoose.SchemaTypes.Url, required: true }, 
+    imageUrl: { type: mongoose.SchemaTypes.Url, required: true },   
+        
 });
 
-NewsItemSchema.plugin(createdPlugin);
+NewsItemSchema.plugin(mongooseCommonPlugin, { object: 'newsitem' });
 
 module.exports = mongoose.model('NewsItem', NewsItemSchema);
