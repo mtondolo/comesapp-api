dff7342330121d7c489387b9aabf9146402bcb8c
mtondolo <mtondolo@gmail.com>
2019-06-06 11:47:32 +0200

    Add email data type with validation

--- api/events/eventsModel/Event.js
+++ api/events/eventsModel/Event.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Schema for events
+const validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const EventSchema = new Schema({
     title: { type: String, required: true },
-    venue: { type: String, required: true },    
+    venue: { type: String, required: true }, 
+    email: {type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']}   
   });
   
   // Get datetime stamp from objectid in milliseconds
