3709276743e514a1de36d2005b11ba03619e34c3
mtondolo <mtondolo@gmail.com>
2019-06-07 15:38:29 +0200

    Convert event date string to long

--- api/events/eventsModel/Event.js
+++ api/events/eventsModel/Event.js
@@ -16,7 +16,7 @@ const EventSchema = new Schema({
       required: 'Email address is required',
       validate: [validateEmail, 'Please fill a valid email address'],
       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
-      startDate: {type: Date, required: true}   
+      startDate: {type: Date, required: true, default: Date.getTime}   
   });
   
   // Get datetime stamp from objectid in milliseconds
